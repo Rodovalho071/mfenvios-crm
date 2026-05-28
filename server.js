666cd1d9da96c778d0e5bda70be623bbf172a69b
Rodovalho071 <contato@mfenvios.com.br>
2026-05-28 17:29:20 -0300

    fix: GET / com sendFile

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const path = require('path');
 const fs = require('fs'); 
 const app = express(); 
 app.use(cors()); 
-app.use(express.json()); 
+app.use(express.json());`nconst path = require('path');`napp.get('/', function(req,res){ res.sendFile(path.join(__dirname,'public','index.html')); }); 
 app.get('/', function(req, res) { res.sendFile(path.join(__dirname, 'public', 'index.html')); }); 
