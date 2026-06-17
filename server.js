fff3d6bdbafcfc9527c3be9559c9843ffdc9f26b
Rodovalho071 <contato@mfenvios.com.br>
2026-06-17 14:27:41 -0300

    fix: PUT kanban remove _id

--- server.js
+++ server.js
@@ -222,12 +222,19 @@ app.patch('/kanban/:id',async function(req,res){
   res.json({ok:true});
 });
 app.put('/kanban/:id',async function(req,res){
-  const lead=Object.assign({},req.body,{id:req.params.id});
+  const body=Object.assign({},req.body);
+  delete body._id; // remover _id do MongoDB se vier no body
+  const lead=Object.assign({},body,{id:req.params.id});
   try{
-    if(kanbanCol){ await kanbanCol.updateOne({id:req.params.id},{$set:lead},{upsert:true}); }
-    else{ const i=memKanban.cards.findIndex(c=>c.id===req.params.id); if(i>=0) memKanban.cards[i]=lead; else memKanban.cards.push(lead); }
+    if(kanbanCol){
+      await kanbanCol.updateOne({id:req.params.id},{$set:lead},{upsert:true});
+    } else {
+      const i=memKanban.cards.findIndex(c=>c.id===req.params.id);
+      if(i>=0) memKanban.cards[i]=Object.assign({},memKanban.cards[i],lead);
+      else memKanban.cards.push(lead);
+    }
     res.json({ok:true});
-  }catch(e){res.status(500).json({error:e.message});}
+  }catch(e){console.error('[PUT kanban]',e.message);res.status(500).json({error:e.message});}
 });
 app.delete('/kanban/:id',async function(req,res){
   try{
